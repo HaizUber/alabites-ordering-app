8c80de5196893aac2bf2106dc84520f6a1998a2a
Meya <gabzmejia117@gmail.com>
2024-04-24 15:15:36 +0800

    revert testing

---
+++ server/models/product.js
@@ -0,0 +1,13 @@
+// server/models/product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  // Add more fields as needed
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
