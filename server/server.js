682b5a6cb0b9e594552c8c9162cb168d1a2e7c44
Meya <gabzmejia117@gmail.com>
2024-04-24 13:51:34 +0800

    test new vercel api handling

--- server/server.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const connectionString = 'mongodb+srv://admin:<password>@alabites.xcgzfpd.mongodb.net/?retryWrites=true&w=majority&appName=alabites';
-
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB', err));
